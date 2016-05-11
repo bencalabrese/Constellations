88d9eed624ee8ec9d3334ef0b0da8f0d388a2311
Ben Calabrese <bencalabrese@gmail.com>
2016-05-11 13:20:46 -0700

    cell toggling

--- cellular_automata.js
+++ cellular_automata.js
@@ -1,10 +1,26 @@
 var Grid = window.Grid;
 
 document.addEventListener('DOMContentLoaded', function() {
-  var canvasEl = document.getElementById('canvas');
-  var ctx = canvasEl.getContext('2d');
-  var grid = new Grid(80, 80);
+  window.canvasEl = document.getElementById('canvas');
+  window.ctx = window.canvasEl.getContext('2d');
+  window.grid = new Grid(80, 80);
 
-  grid.render(ctx);
-  console.log('hello world');
+
+  window.grid.awakenCells([
+    [3,3],
+    [3,4],
+    [4,3],
+    [4,4],
+
+    [10,3],
+    [10,4],
+    [10,5]
+  ]);
+
+  setInterval(function() {
+    window.grid.render(window.ctx);
+  }, 250);
+
+  
+
 });
