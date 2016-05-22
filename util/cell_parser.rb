9f8f10a7b9f64890f3760e824b577b50602deac7
Ben Calabrese <bencalabrese@gmail.com>
2016-05-22 16:14:54 -0700

    add parser for .cells files

---
+++ util/cell_parser.rb
@@ -0,0 +1,34 @@
+# Utility to parse text from conwaylife.com .cells files
+
+def parse_live_cells(lines)
+  live_cells = []
+  height = 0
+  width = 0
+
+  lines.each_with_index do |line, col|
+    line.split("").each_with_index do |cell, row|
+      if cell == "O"
+        live_cells << [row + 1, col + 1]
+        width = row if row > width
+        height = col if col > height
+      end
+    end
+  end
+
+  height += 2
+  width += 2
+
+  live_cells.map! { |cell| "    " + cell.to_s }
+  deltas = live_cells.join(",\n")
+
+  "  height: #{height},\n  width: #{width},\n  liveCellDeltas: [\n#{deltas}\n  ]"
+end
+
+lines = File.readlines(File.join(File.dirname(__FILE__), 'cell_map.txt'))
+
+lines.reject! { |line| line[0] == "!" }
+lines.map!(&:chomp)
+
+File.open(File.join(File.dirname(__FILE__), 'output.txt'), "w") do |f|
+  f.write parse_live_cells(lines)
+end
